feec2eb095e4c1d9170fe30534a745eefcba0186
刘东才 <liudongcai@hotmail.com>
2025-03-20 10:24:26 +0800

    auto commit

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,7 @@
 import { defineConfig } from "vite";
 import react from '@vitejs/plugin-react'
 import { fileURLToPath, URL } from "node:url";
-import externalGlobals from "rollup-plugin-external-globals";
 import { createHtmlPlugin } from "vite-plugin-html";
-import fs from "fs";
-import path from "path";
 // eslint-disable-next-line @typescript-eslint/no-require-imports
 const pkg = require("./package.json");
 // const dist = `dist/${pkg.name}`;
@@ -21,20 +18,6 @@ export default defineConfig({
         },
       },
     }),
-    {
-      name: "app.json",
-      writeBundle() {
-        const destPath = path.resolve(__dirname, dist, "app.json");
-        fs.writeFileSync(
-          destPath,
-          JSON.stringify({
-            title: pkg.title || pkg.name,
-            flag: pkg.flag,
-            desc: pkg.description,
-          })
-        );
-      },
-    },
   ],
   base: "./",
   resolve: {
@@ -43,30 +26,14 @@ export default defineConfig({
     },
   },
   build: {
-    rollupOptions: {
-      plugins: [
-        externalGlobals({
-          react: 'React',
-          'react-dom': 'ReactDOM',
-        }),
-      ],
-    },
     outDir: dist,
   },
   server: {
     proxy: {
-      "/userinfo": {
-        target: "https://it.bright-ai.com",
+      "/test": {
+        target: "https://test.bright-ai.com",
         secure: false,
-      },
-      "/logout": {
-        target: "https://it.bright-ai.com",
-        secure: false,
-      },
-      "/login": {
-        target: "https://it.bright-ai.com",
-        secure: false,
-      },
+      }
     },
   },
 });
